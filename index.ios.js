08978b11a7d1a48048e68ec8484e84c44c3f5697
David Ladowitz <david@ladowitz.com>
2016-12-13 20:55:07 -0800

    Search multiple locations

--- index.ios.js
+++ index.ios.js
@@ -10,35 +10,52 @@ import OAuthSimple from 'oauthsimple';
 
 export default class food_trip extends Component {
   state = {
-    position: 'unknown'
+    position: 'unknown',
+    businesses: [],
+    waypoints: ['720 Jones st, San Francisco, CA', '160 Visitacion Ave, Brisbane, CA', '340 University Ave, Palo Alto, CA', '18324 Clemson Ave, Saratoga, CA']
   };
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('Position: ', position);
-        this.setState({ position });
-      },
-      (error) => alert(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
+    console.log('componentDidMount....');
+    console.log('State: ', this.state);
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     console.log('Position: ', position);
+    //     this.setState({ position });
+    //   },
+    //   (error) => alert(error),
+    //   { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    // );
   }
 
-  fetchData() {
-    const lat = this.state.position.coords.latitude;
-    const lng = this.state.position.coords.longitude;
-    const latlng = `ll=${String(lat)},${String(lng)}`;
+  findFoodOnRoute() {
+    this.state.waypoints.map((waypoint) => {
+      console.log('Looking for food around: ', waypoint)
+      return this.fetchData(waypoint);
+    });
+  }
+
+  fetchData(city) {
+    // const lat = this.state.position.coords.latitude;
+    // const lng = this.state.position.coords.longitude;
+    // const latlng = `ll=${String(lat)},${String(lng)}`;
     const consumerKey = 'nVeyIROhiW6EMFBcA5iA1w';
     const consumerSecret = 'GrP921_svILXGljhQSnm1_tMBX8';
     const tokenSecret = '9XpvW154RPZxgB-LPcI5llrs47U';
     const token = 'S5dzlOqQCjgMptscu9cZTG_yzyFaAyvq';
+    const searchTerm = 'ramen';
+    const location = city;
+    const radius = '2000';
+    const limit = 5;
+    const openNow = true;
+    const params = `term=${searchTerm}&location=${location}&radius=${radius}&limit=${limit}&open_now=${openNow}`;
     // const nav = this.props.navigator;
 
     const oauth = new OAuthSimple(consumerKey, tokenSecret);
     const request = oauth.sign({
       action: 'GET',
       path: 'https://api.yelp.com/v2/search',
-      parameters: `term=ramen&${latlng}`,
+      parameters: params,
       signatures: { api_key: consumerKey,
                     shared_secret: consumerSecret,
                     access_token: token,
@@ -49,11 +66,15 @@ export default class food_trip extends Component {
     fetch(request.signed_url, { method: 'GET' }).then((response) => {
       return response.json();
     }).then((data) => {
-      console.log('Data: ', data);
-      // navigator.push({
-      //   ident: 'Results',
-      //   data: data
-      // });
+      let newBusinesses = this.state.businesses;
+
+      data.businesses.map((business) => {
+        return newBusinesses.push(business);
+      });
+
+      this.setState({ businesses: newBusinesses });
+      console.log('Businesses: ', this.state.businesses);
+
     }).catch((error) => {
       console.log('Error:', error);
     });
@@ -68,7 +89,8 @@ export default class food_trip extends Component {
         </Text>
         <TouchableOpacity
           style={{ borderRadius: 7, padding: 10, backgroundColor: 'rgb(37, 160, 205)' }}
-          onPress={this.fetchData.bind(this)}
+          // onPress={this.fetchData.bind(this)}
+          onPress={this.findFoodOnRoute.bind(this)}
         >
           <Text style={{ fontSize: 15 }}>Find Food!</Text>
 
